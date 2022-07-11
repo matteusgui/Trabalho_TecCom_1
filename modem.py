370c30fae6ec53986ac5622ea8a6103320254e15
Fernando Kiyoshi Kaida <52013484+FKKAIDA@users.noreply.github.com>
2022-07-11 15:04:56 -0300

    10 do fernandao

--- modem.py
+++ modem.py
@@ -1,22 +1,19 @@
 import numpy as np
 import scipy.signal
 
-
 class Modem:
     def __init__(self, fs, bufsz, ans=False):
         self.bit_rate = 300
-        self.lastn = 0
-        self.lastnlist = []
         self.fi = 0
         self.bits =[]
         self.s = []
-        self.sbuffer=[]
-        self.v0i=np.zeros(bufsz)
-        self.v0r=np.zeros(bufsz)
-        self.v1i=np.zeros(bufsz)
-        self.v1r=np.zeros(bufsz)
-        self.y = []
-        self.v = []
+        self.sbuffer= np.zeros(fs//self.bit_rate).tolist()
+        self.v0i= 0
+        self.v0r= 0
+        self.v1i= 0
+        self.v1r= 0
+        self.y = None
+        self.v = [0, 0, 0]
         self.fs = fs  # taxa de amostragem
         self.bufsz = bufsz  # quantidade de amostas que devem ser moduladas por vez
         # frequências de modulação (upload)
@@ -57,134 +54,94 @@ class Modem:
     # Demodulação
 
     def put_samples(self, data):
-        self.s = np.append(self.s,data)
+        self.s = self.s.copy() + data.tolist()
 
     def get_bits(self):
-        s = []
-        s.extend(self.sbuffer)
-        if len(s) == 0:
-            s.extend(np.zeros(self.bufsz).tolist())
-        s.extend(self.s)
-        self.sbuffer= s[len(s)-self.bufsz:]
+        
 
         fs= self.fs
         T = 1/fs
-        L = fs//300
+        L = fs//self.bit_rate #self.bit_rate = 300
         pi = np.pi
-        omega0=self.rx_omega0
+        omega0=self.rx_omega0 
         omega1=self.rx_omega1
 
-        plotar = len(self.s)<20*160
-        tam = len(self.v0i)
         #print("Tam = ",tam)
 
-        self.v0i = np.append(self.v0i,np.zeros(len(self.s)))
-        self.v0r = np.append(self.v0r,np.zeros(len(self.s)))
-        self.v1i = np.append(self.v1i,np.zeros(len(self.s)))
-        self.v1r = np.append(self.v1r,np.zeros(len(self.s)))
+        cutoff_index = len(self.s) - len(self.s)%L
+        if(cutoff_index < L):
+            return []
+        
+        s = self.sbuffer + self.s[:cutoff_index]
+        leftover_s = self.s[cutoff_index:]
 
-        v0i=self.v0i
-        v0r=self.v0r
-        v1i=self.v1i
-        v1r=self.v1r
+        v0i= self.v0i
+        v0r= self.v0r
+        v1i= self.v1i
+        v1r= self.v1r
+
+        V0i= np.zeros(len(s) - L)
+        V0r= np.zeros(len(s) - L)
+        V1i= np.zeros(len(s) - L)
+        V1r= np.zeros(len(s) - L)
 
         r=0.99
 
         #print("len s=", len(s))
         #print("len v0=", len(v0i),len(v0r),len(v1i),len(v1r))
 
-        for n in range(tam,len(s)):
-                v0r[n] = s[n] - r**L * np.cos(omega0*L*T)*s[n-L] + r*np.cos(omega0*T)*v0r[n-1] - r*np.sin(omega0*T)*v0i[n-1]
-                v0i[n] = -r**L*np.sin(omega0*L*T)*s[n-L] + r*np.cos(omega0*T)*v0i[n-1] + r*np.sin(omega0*T)*v0r[n-1]
-                v1r[n] = s[n] - r**L * np.cos(omega1*L*T)*s[n-L] + r*np.cos(omega1*T)*v1r[n-1] - r*np.sin(omega1*T)*v1i[n-1]
-                v1i[n] = -r**L*np.sin(omega1*L*T)*s[n-L] + r*np.cos(omega1*T)*v1i[n-1] + r*np.sin(omega1*T)*v1r[n-1]
+        for n in range(L,len(s)):
+            V0r[n-L] = s[n] - r**L * np.cos(omega0*L*T)*s[n-L] + r*np.cos(omega0*T)*v0r - r*np.sin(omega0*T)*v0i
+            V0i[n-L] = -r**L*np.sin(omega0*L*T)*s[n-L] + r*np.cos(omega0*T)*v0i + r*np.sin(omega0*T)*v0r
+            V1r[n-L] = s[n] - r**L * np.cos(omega1*L*T)*s[n-L] + r*np.cos(omega1*T)*v1r - r*np.sin(omega1*T)*v1i
+            V1i[n-L] = -r**L*np.sin(omega1*L*T)*s[n-L] + r*np.cos(omega1*T)*v1i + r*np.sin(omega1*T)*v1r
 
-        rho = v1r**2+v1i**2+v0r**2+v0i**2   # carrier detection
+            v0r = V0r[n-L]
+            v0i = V0i[n-L]
+            v1r = V1r[n-L]
+            v1i = V1i[n-L]
+        
+        self.v0i = v0i
+        self.v0r = v0r
+        self.v1i = v1i
+        self.v1r = v1r
 
-        c = abs(v1r**2+v1i**2-v0r**2-v0i**2)
-        v = np.zeros(len(c))
+        rho = V1r**2+V1i**2+V0r**2+V0i**2   # carrier detection
+
+        c = abs(V1r**2+V1i**2-V0r**2-V0i**2)
+        v = self.v.copy()
 
         
-        #self.v = np.append(self.v,np.zeros(len(self.s)))
 
-        if len(self.v)>0:
-            v[0] = self.v[0]
-            v[1] = self.v[1]
         y = np.zeros(len(c))
-        if len(self.y)>0:
-            y[0] = self.y[0]
-            y[1] = self.y[1]
         
         r = 0.9999
-        for n in range(2,len(c)):
-                v[n] = (1-r)*c[n] + 2*r*np.cos(2*pi*300/fs)*v[n-1] - r**2*v[n-2]
-                y[n] = v[n] - v[n-2]
+        for n in range(len(c)):
+                v[2] = (1-r)*c[n] + 2*r*np.cos(2*pi*300/fs)*v[1] - r**2*v[0]
+                y[n] = v[2] - v[0]
 
-        self.v = v[len(v)-2:]
-        #print("len de v = ", len(self.v))
-        self.y = y[len(y)-2:]
-        #if plotar:
-            #plt.plot(y,'r')
-            #plt.plot(v,'g')
-        delta = v1r**2+v1i**2-v0r**2-v0i**2
-        #print(delta)
-        #plt.plot(delta,'r')
+                v[0] = v[1]
+                v[1] = v[2]
 
+        self.v = v.copy()
 
-        filt=scipy.signal.firwin(40, 300, pass_zero='lowpass', fs=self.fs)
+        #print(f"help me = {self.y}")
+        if self.y != None:
+            y = np.concatenate((self.y, y))
+        
+        delta = V1r**2+V1i**2-V0r**2-V0i**2
+
         ponto_amostra = 1500*((y[1:]>0)&(y[:-1]<0))
-        #ponto_amostra = np.append(ponto_amostra,[0])
+
         bits = []
-        #plt.plot(ponto_amostra)
-        #delta = np.convolve(delta, filt)
-        #print("len amostra=",len(ponto_amostra))
-        #print ("len delta =",len(delta))
-        #if(sum(ponto_amostra)!= 3000):
-            #print("sum = ", sum(ponto_amostra))
-
-
         for i in range (len(ponto_amostra)):
-            if ponto_amostra[i] != 0 and not self.lastnlist.count(i):# and not plotar or plotar and i == 2*self.bufsz-50:
-                bits.append(1 if delta[i] > 0 else 0)
-                for j in range (-self.bufsz//4,self.bufsz//4+1):
-                    self.lastnlist.append(i+j)
-            #if(i==2*self.bufsz-50):
-                #print(i,"=>",y[i])
-                #print(" ") 
-            #if(i==self.bufsz-50):
-                #print(i,"=>",y[i])
+            if ponto_amostra[i] != 0:
+                bits.append(1 if delta[i] > 0.5 else 0)
+        #for i in range(L//2 + 46, len(delta), L):
+        #   bits.append(1 if delta[i] > 0.5 else 0)
 
-        for i in range (len(self.lastnlist)):
-            self.lastnlist[i] = self.lastnlist[i]-self.bufsz
-
-        #print("lastnlist = ", self.lastnlist)
-
-        if(len(self.lastnlist)>0):
-            while(len(self.lastnlist)>0 and self.lastnlist[0]<0):
-                self.lastnlist.pop(0)
-
-
-        #print("bits =",bits)
-        #print("len(self.s)=",len(self.s))
-        #bits = bits[:]
-        self.s = []
-        #print("bits =",bits)
-        self.v0i = v0i[len(v0i)-self.bufsz:]
-        self.v0r = v0r[len(v0r)-self.bufsz:]
-        self.v1i = v1i[len(v1i)-self.bufsz:]
-        self.v1r = v1r[len(v1r)-self.bufsz:]
-        #self.s = s[:-self.bufsz]
-
-        #bits = bits[self.lastn:]
-        #self.lastn = 0 if len(bits) == 0 else 1
-
-        #print(bits)
-        #print("bits = ", bits)
-        #if(plotar):
-            #plt.plot(delta,'r')
-            #plt.plot(ponto_amostra,'g')
-            #plt.show()
-
-        #plt.show()
+        self.s = leftover_s.copy()
+        self.sbuffer = s[len(s)-L:] 
+        self.y = np.array([y[-1]])
 
         return bits
